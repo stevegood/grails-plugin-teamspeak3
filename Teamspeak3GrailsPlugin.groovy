28ce967adb2af35dfec77f17a4c6397395f1f02a
Steve Good <steve@stevegood.org>
2014-05-11 17:19:49 -0500

    Updated plugin meta info

--- Teamspeak3GrailsPlugin.groovy
+++ Teamspeak3GrailsPlugin.groovy
@@ -10,19 +10,19 @@ class Teamspeak3GrailsPlugin {
 
     // TODO Fill in these fields
     def title = "Teamspeak3 Plugin" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def author = "Steve Good"
+    def authorEmail = "steve@stevegood.org"
     def description = '''\
-Brief summary/description of the plugin.
+Adds integration between a Grails application and a TeamSpeak3 server
 '''
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/teamspeak3"
+    def documentation = "https://github.com/stevegood/grails-plugin-teamspeak3"
 
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "GPL2"
 
     // Details of company behind the plugin (if there is one)
 //    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
@@ -31,10 +31,10 @@ Brief summary/description of the plugin.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "GitHub", url: "https://github.com/stevegood/grails-plugin-teamspeak3/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/stevegood/grails-plugin-teamspeak3" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
