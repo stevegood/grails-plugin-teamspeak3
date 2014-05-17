fe69141e1b982eadbdf95d49956a8108bd1059fa
Steve Good <steve@stevegood.org>
2014-05-16 22:20:04 -0500

    Added additional helper methods.

--- grails-app/services/org/stevegood/teamspeak3/TeamSpeakService.groovy
+++ grails-app/services/org/stevegood/teamspeak3/TeamSpeakService.groovy
@@ -17,6 +17,9 @@ class TeamSpeakService {
     TS3Api ts3Api
     def grailsApplication
     private boolean connected = false
+    private TS3Listener chatListener
+    private TS3Listener joinListener
+    private TS3Listener leaveListener
 
     @PostConstruct
     void init() {
@@ -38,21 +41,20 @@ class TeamSpeakService {
 
             ts3Api = ts3Query.api
             ts3Api.selectVirtualServerById(1)
+            ts3Api.nickname = config.nick
             println 'init complete!'
         }
     }
 
     def initChatBot(Closure closure) {
-        def config = grailsApplication.mergedConfig.grails.plugin.teamspeak3
-        ts3Api.nickname = config.nick
-
-        ts3Api.unregisterAllEvents()
+        if (chatListener)
+            ts3Api.removeTS3Listeners(chatListener)
 
         Closure closureClone = closure.clone()
         closureClone.delegate = this
         closureClone.resolveStrategy = Closure.DELEGATE_FIRST
 
-        TS3Listener ts3Listener = new TS3Listener() {
+        chatListener = new TS3Listener() {
             @Override
             void onTextMessage(TextMessageEvent textMessageEvent) {
                 closureClone(textMessageEvent)
@@ -89,16 +91,95 @@ class TeamSpeakService {
             }
         }
 
+        ts3Api.registerEvent(TS3EventType.TEXT_PRIVATE)
+        ts3Api.registerEvent(TS3EventType.TEXT_SERVER)
         ts3Api.channels.each { Channel channel ->
             ts3Api.registerEvent(TS3EventType.TEXT_CHANNEL, channel.id)
-            ts3Api.registerEvent(TS3EventType.TEXT_PRIVATE, channel.id)
         }
 
-        ts3Api.addTS3Listeners(ts3Listener)
+        ts3Api.addTS3Listeners(chatListener)
+    }
+
+    def onServerLeave(Closure closure) {
+        if (leaveListener)
+            ts3Api.removeTS3Listeners(leaveListener)
+
+        Closure runClosure = closure.clone()
+        runClosure.delegate = this
+        runClosure.resolveStrategy = Closure.DELEGATE_FIRST
+
+        leaveListener = new TS3Listener() {
+            @Override
+            void onTextMessage(TextMessageEvent textMessageEvent) {}
+
+            @Override
+            void onClientJoin(ClientJoinEvent clientJoinEvent) {}
+
+            @Override
+            void onClientLeave(ClientLeaveEvent clientLeaveEvent) {
+                runClosure clientLeaveEvent
+            }
+
+            @Override
+            void onServerEdit(ServerEditedEvent serverEditedEvent) {}
+
+            @Override
+            void onChannelEdit(ChannelEditedEvent channelEditedEvent) {}
+
+            @Override
+            void onChannelDescriptionChanged(ChannelDescriptionEditedEvent channelDescriptionEditedEvent) {}
+
+            @Override
+            void onClientMoved(ClientMovedEvent clientMovedEvent) {}
+        }
+
+        ts3Api.registerEvent(TS3EventType.SERVER)
+        ts3Api.addTS3Listeners leaveListener
+    }
+
+    def onServerJoin(Closure closure) {
+        if (joinListener)
+            ts3Api.removeTS3Listeners(joinListener)
+
+        Closure runClosure = closure.clone()
+        runClosure.delegate = this
+        runClosure.resolveStrategy = Closure.DELEGATE_FIRST
+
+        joinListener = new TS3Listener() {
+            @Override
+            void onTextMessage(TextMessageEvent textMessageEvent) {}
+
+            @Override
+            void onClientJoin(ClientJoinEvent clientJoinEvent) {
+                runClosure clientJoinEvent
+            }
+
+            @Override
+            void onClientLeave(ClientLeaveEvent clientLeaveEvent) {}
+
+            @Override
+            void onServerEdit(ServerEditedEvent serverEditedEvent) {}
+
+            @Override
+            void onChannelEdit(ChannelEditedEvent channelEditedEvent) {}
+
+            @Override
+            void onChannelDescriptionChanged(ChannelDescriptionEditedEvent channelDescriptionEditedEvent) {}
+
+            @Override
+            void onClientMoved(ClientMovedEvent clientMovedEvent) {}
+        }
+
+        ts3Api.registerEvent(TS3EventType.SERVER)
+        ts3Api.addTS3Listeners joinListener
     }
 
     def getClients() {
         ts3Api.clients
+    }
+
+    def sendServerMessage(String message) {
+        ts3Api.sendServerMessage message
     }
 
     def sendMessage(String message) {
@@ -106,7 +187,7 @@ class TeamSpeakService {
     }
 
     def sendPrivateMessage(int clientId, String message) {
-        ts3Api.sendPrivateMessage clientId, message
+        ts3Api.sendPrivateMessage(clientId, message)
     }
 
     def broadcastMessage(String message) {
